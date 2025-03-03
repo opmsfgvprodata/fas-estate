6f9afb53cffc8ef4c62a4787f26e23a20da3de1f
opmsfgvprodata <opms.fgvprodata@gmail.com>
2025-03-04 04:44:03 +0800

    Update Connection.cs

--- MVC_SYSTEM/Class/Connection.cs
+++ MVC_SYSTEM/Class/Connection.cs
@@ -17,10 +17,6 @@ namespace MVC_SYSTEM.Class
             catalog = getconnection.InitialCatalog;
             user = getconnection.userID;
             pass = getconnection.Password;
-
-            //debug prod
-            host = "4.144.178.156,11433";
-
         }
 
         public string GetConnectionString(int? wlyhID, int? syrktID, int? ngrID)
@@ -28,12 +24,9 @@ namespace MVC_SYSTEM.Class
             MVC_SYSTEM_MasterModels db = new MVC_SYSTEM_MasterModels();
             var getConnection = db.tblConnections.Where(x => x.wilayahID == wlyhID && x.syarikatID == syrktID && x.negaraID == ngrID && x.deleted == false).FirstOrDefault();
             var host = getConnection.DataSource;
-            //var host = getConnection.DataSourceInternal;
             var catalog = getConnection.InitialCatalog;
             var user = getConnection.userID;
             var pass = getConnection.Password;
-            //debug prod
-            host = "4.144.178.156,11433";
             var connectionString = String.Format("data source={0};initial catalog={1};user id={2};password={3};MultipleActiveResultSets=True;App=EntityFramework;Connection Timeout=300", host, catalog, user, pass);
             return connectionString;
         }
